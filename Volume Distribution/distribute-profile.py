0019045c94a85b32a9486002cc118877856b874b
maruvici <mvtevangelista0820@gmail.com>
2025-02-25 14:22:27 +0800

    Create distribute-profile.py

---
+++ Volume Distribution/distribute-profile.py
@@ -0,0 +1,268 @@
+# adapted from previous iteration's implementation
+# https://github.com/mylescoding/ssl07-2324/blob/main/README.md
+
+import random
+
+class Source:
+    def __init__(self, source_name, possible_routes):
+        self.source_name = source_name
+        self.possible_routes = possible_routes
+        
+    def getSourceName(self):
+        return self.source_name
+    def getPossibleRoutes(self):
+        return self.possible_routes
+
+class Intermediate:
+    def __init__(self, node_name, possible_sources):
+        self.node_name = node_name
+        self.possible_sources = possible_sources
+    def getIntermediate_Node_Name(self):
+        return self.node_name
+    def getPossibleSources(self):
+        return self.possible_sources
+
+count = 0
+def createXML_hourly_profiled(dic, idx):
+    global count
+    xml_entries = []
+    #assume ratio between motorcycles to cars is  3:2
+    start_end_times = [('0.0', '3600.0'), ('3600.0', '7200.0'), ('7200.0', '10800.0'), ('10800.0', '14400.0'), ('14400.0', '18000.0'), ('18000.0', '21600.0'), ('21600.0', '25200.0'), ('25200.0', '28800.0'), ('28800.0', '32400.0'), ('32400.0', '36000.0'), ('36000.0', '39600.0'), ('39600.0', '43200.0'), ('43200.0', '46800.0'), ('46800.0', '50400.0')]
+    
+    for key in dic:
+        motor_count = int(round(dic[key] * 0.6))
+        car_count = int(round(dic[key] * 0.4))
+        xml_entry_motor = '<flow id ="f_' + str(count) +'"' + ' type = "phMotor" begin="'  + str(start_end_times[idx - 5][0]) + '0" route="' + str(key) + '" end="'+ str(start_end_times[idx - 5][1]) + '0" number="'+ str(motor_count) +'"'+' departLane="free" departSpeed="max"'+'/>' 
+        count = count + 1
+        xml_entry_car = '<flow id ="f_' + str(count) +'"' + ' type = "phCar" begin="'  + str(start_end_times[idx - 5][0]) + '0" route="' + str(key) + '" end="'+ str(start_end_times[idx - 5][1]) + '0" number="'+ str(car_count) +'"'+' departLane="free" departSpeed="max"'+'/>' 
+        count = count + 1
+        if motor_count != 0:
+            xml_entries.append(xml_entry_motor)
+        else:
+            count = count - 1
+        if car_count != 0:
+            xml_entries.append(xml_entry_car)
+        else:
+            count = count - 1
+
+    for entry in xml_entries:
+        print(entry)
+        pass
+
+    return
+def createXML_wholeday_profiled(dic):
+    count = 0
+    xml_entries = []
+    #assume ratio between motorcycles to cars is  3:2
+    start_end_times = [('0.0', '3600.0'), ('3600.0', '7200.0'), ('7200.0', '10800.0'), ('10800.0', '14400.0'), ('14400.0', '18000.0'), ('18000.0', '21600.0'), ('21600.0', '25200.0'), ('25200.0', '28800.0'), ('28800.0', '32400.0'), ('32400.0', '36000.0'), ('36000.0', '39600.0'), ('39600.0', '43200.0'), ('43200.0', '46800.0'), ('46800.0', '50400.0')]
+    for idx in range (4):
+        for key in dic:
+            motor_count = int(round(dic[key] * 0.6))
+            car_count = int(round(dic[key] * 0.4))
+            xml_entry_motor = '<flow id ="f_' + str(count) +'"' + ' type = "phMotor" begin="'  + str(start_end_times[idx][0]) + '0" route="' + str(key) + '" end="'+ str(start_end_times[idx][1]) + '0" number="'+ str(motor_count) +'"/>' 
+            count = count + 1
+            xml_entry_car = '<flow id ="f_' + str(count) +'"' + ' type = "phCar" begin="'  + str(start_end_times[idx][0]) + '0" route="' + str(key) + '" end="'+ str(start_end_times[idx][1]) + '0" number="'+ str(car_count) +'"/>' 
+            count = count + 1
+            if motor_count != 0:
+                xml_entries.append(xml_entry_motor)
+            else:
+                count = count - 1
+            if car_count != 0:
+                xml_entries.append(xml_entry_car)
+            else:
+                count = count - 1
+
+    for entry in xml_entries:
+        print(entry)
+        pass
+
+    return
+
+
+def distribute(time_slot):
+
+    for source_node in sources:
+        sum_out_src = 0
+        demand_sum = 0
+        if (source_node.getPossibleRoutes() != []):
+            for intermediate_node in intermediates:  
+                if (intermediate_node.getIntermediate_Node_Name() in source_node.getPossibleRoutes()):
+                    #print( source_node.getPossibleRoutes()) 
+                    # kbt5: ["kuf4", "kuf4a_kbt2", "kuf4a_kbt3", "kuf4b", "kuf5"]
+                    #print("Intermediate: " + str(intermediate_node.getIntermediate_Node_Name()))
+                    #sum_out_src += intermediate_demand_dict[intermediate_node.getIntermediate_Node_Name()]
+                    sum_out_src += intermediate_demand_dict_wholeday[intermediate_node.getIntermediate_Node_Name()][time_slot]
+                    #print("sum_out_src is:" + str(sum_out_src))
+            #print("OKAY SLAAAYYYY")
+            for route in source_node.getPossibleRoutes():
+                #once we get the some, get ratio
+                ratio = float(intermediate_demand_dict_wholeday[route][time_slot] / sum_out_src)
+                #print("source is: " + source_node.getSourceName() +" with demand of " + str(source_demand_dict_wholeday[source_node.getSourceName()][time_slot])+ " while route is: " + str(intermediate_demand_dict_wholeday[route][time_slot]))
+                #print("ratio:" + str(ratio))
+                demand = round(float(source_demand_dict_wholeday[source_node.getSourceName()][time_slot] * ratio))
+                demand_sum += demand
+                new_route_name = str(source_node.getSourceName()) + "_to_" + str(route)
+                route_dict[new_route_name] = demand    
+                route_name_list.append(new_route_name)
+            #print("demand_sum is: " + str(demand_sum) + " actual: " + str(source_demand_dict_wholeday[source_node.getSourceName()][time_slot]))
+        elif(source_node.getPossibleRoutes() == []):
+            route_dict[source_node.getSourceName()] = source_demand_dict_wholeday[source_node.getSourceName()][time_slot]
+            route_name_list.append(source_node.getSourceName())
+    return
+
+source_demand_dict_wholeday = {
+        'ka-1': [],
+        'ka-1ab': [],
+        'ka-3': [], 
+        'ka-8': [],
+        'ka-9': [],
+        'ka-10': [],
+        'ka-11': [],
+        'ka-12': [],
+        'kx-9': [],
+        'kuf-7': [],
+        'kuf-8': [], 
+        'kuf-8a': [], 
+        'kuf-9': [],
+        'kuf-10b': [], 
+        'kuf-10c': [], 
+        'kuf-12a': [], 
+        'kbt-4': [], 
+        'kbt-4a': [], 
+        'kbt-5': [], 
+        'kbt-6': [], 
+        'kbt-7': [], 
+        'kbt-8': [], 
+        'kbt-9': [], 
+        'kbt-10': [], 
+        'kbt-11': [], 
+        'kbt-12': []
+    }
+    
+intermediate_demand_dict_wholeday = {
+        'ka-4': [],
+        'ka-4ab_to_kuf-2': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'ka-4ab_to_kuf-3': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'ka-4ab_to_kuf-3a': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'ka-5a': [],
+        'ka-6': [],
+        'kx-6': [],
+        'kuf-2': [],
+        'kuf-3': [],
+        'kuf-3a': [],
+        'kuf-4': [],
+        'kuf-4a': [],
+        'kuf-4a_to_kbt-2': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'kuf-4a_to_kbt-3': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        'kuf-4b': [],
+        'kuf-5': [],
+        'kbt-2': [],
+        'kbt-3': []
+    }
+
+# split demand from kuf-4a to both kbt-2 and kbt-3
+for idx in range(14):
+    x = random.randint(0,intermediate_demand_dict_wholeday['kuf-4a'][idx])
+    y = intermediate_demand_dict_wholeday['kuf-4a'][idx] - x 
+    intermediate_demand_dict_wholeday['kuf-4a_to_kbt-2'][idx] = x
+    intermediate_demand_dict_wholeday['kuf-4a_to_kbt-3'][idx] = y
+
+#Source Nodes
+
+#KA
+ka1 = Source("ka-1", [])
+ka1ab = Source("ka-1ab", [])
+ka2a = Source("ka-2a", []) # edit this later
+ka3 = Source("ka-3", [])
+ka8 = Source ("ka-8", [])
+ka9 = Source ("ka-9", [])
+ka10 = Source("ka-10", [])
+ka11 = Source("ka-11", [])
+ka12 = Source("ka-12", []) # edit this later
+
+#KX
+kx9 = Source("kx-9", []) # edit this later
+
+#KUF
+kuf7 = Source("kuf-7", []) # edit this later
+kuf8 = Source("kuf-8", [])
+kuf8a = Source("kuf-8a", [])
+kuf9 = Source("kuf-9", []) # edit this later
+kuf10b = Source("kuf-10b", []) # edit this later
+kuf10c = Source("kuf-10c", []) # edit this later
+kuf12a = Source("kuf-12a", []) # edit this later
+
+#KBT
+kbt4 = Source("kbt-4",[])
+kbt4a = Source("kbt-4a",[])
+kbt5 = Source("kbt-5",[]) # edit this later
+kbt6 = Source("kbt-6", []) 
+kbt7 = Source("kbt-7",[])
+kbt8 = Source("kbt-8",[])
+kbt9 = Source("kbt-9",[]) # edit this later
+kbt10 = Source("kbt-10", []) # edit this later
+kbt11 = Source("kbt-11", [])
+kbt12 = Source("kbt-12",[])
+
+sources = [ka1, ka1ab, ka3, ka8, ka9, ka10, ka11, ka12, kx9, kuf7, kuf8, kuf8a, kuf9, kuf10b, kuf10c, kuf12a, kbt4, kbt4a, kbt5, kbt6, kbt7, kbt8, kbt9, kbt10, kbt11, kbt12]
+
+#Intermediate nodes
+
+#KA
+ka4 = Intermediate("kuf-4",[])
+ka4ab_kuf2 = Intermediate("kuf-4ab_to_kuf-2", []) #edit this later
+ka4ab_kuf3 = Intermediate("kuf-4ab_to_kuf-3",[]) #edit this later
+ka4ab_kuf3a = Intermediate("ka-4ab_to_kuf-3a", []) #edit this later
+ka5a = Intermediate("ka-5a", [])
+ka6 = Intermediate("ka-6",[])
+
+#KX
+kx6 = Intermediate("kx-6", []) #edit this later
+
+#KUF
+kuf2 = Intermediate("kuf-2",[]) # edit this later
+kuf3 = Intermediate("kuf-3",[]) # edit this later
+kuf3a = Intermediate("kuf-3a",[]) # edit this later
+kuf4 = Intermediate("kuf-4",["kbt-5","kbt-9","kbt-10"])
+kuf4a_kbt2 = Intermediate("kuf-4a_to_kbt-2", ["kbt-5","kbt-9","kbt-10"])
+kuf4a_kbt3 = Intermediate("kuf-4a_to_kbt-3", ["kbt-5","kbt-9","kbt-10"])
+kuf4b = Intermediate("kuf-4b", ["kbt-5","kbt-9","kbt-10"])
+kuf5 = Intermediate("kuf-5", ["kbt-5","kbt-9","kbt-10"])
+
+#KBT
+kbt2 = Intermediate("kbt-2",[]) # edit this later
+kbt3 = Intermediate("kbt-3",[]) # edit this later
+
+intermediates = [ka4, ka4ab_kuf2, ka4ab_kuf3, ka4ab_kuf3a, ka5a, ka6, kx6, kuf2, kuf3, kuf3a, kuf4, kuf4a_kbt2, kuf4a_kbt3, kuf4b, kuf5, kbt2, kbt3]
+
+route_dict = {}
+route_name_list=[]
+
+#distribute(0)
+#createXML_hourly_profiled(route_dict, 0)
+
+def sum_indices(dictionary):
+    # Initialize a list to store sums for each index
+    sums = [0] * 14
+    
+    # Iterate over the dictionary
+    for key, value_list in dictionary.items():
+        # Iterate over the list and sum the values at each index
+        for i in range(14):
+            sums[i] += value_list
+    
+    return sums
+
+for idx in range(14):
+   distribute(idx)
+   swank = sum_indices(route_dict)
+   print(swank)
+
+
+   createXML_hourly_profiled(route_dict, idx)
+   print(route_dict)
+   print(sum(route_dict.values()))
+
+createXML_wholeday_profiled(route_dict)
+print
+
